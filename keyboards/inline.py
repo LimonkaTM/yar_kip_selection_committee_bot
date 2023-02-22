0926f82e4036bf7f039909251d1147f3dd279370
LimonkaTM <etuzin@mail.ru>
2023-02-23 01:25:00 +0300

    fix imports in keyboards/inline

--- keyboards/inline.py
+++ keyboards/inline.py
@@ -1,14 +1,17 @@
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
-from lexicon.lexicon import LEXICON_RU
+from lexicon.lexicon import LEXICON_BUTTONS
 
 
 def create_main_menu_kb(*buttons_keys: str) -> InlineKeyboardMarkup:
+    # создаём билдер клавиатуры
     kb_builder: InlineKeyboardBuilder = InlineKeyboardBuilder()
+    # добавляем строки с кнопками
     kb_builder.row(*[InlineKeyboardButton(
-        text=LEXICON_RU['buttons'][button]
-        if button in LEXICON_RU['buttons'] else button,
+        text=LEXICON_BUTTONS[button]
+        if button in LEXICON_BUTTONS else button,
         callback_data=button) for button in buttons_keys],
         width=1)
+    # возвращаем объект клавиатуры с кнопками
     return kb_builder.as_markup()

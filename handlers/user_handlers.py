3651e7aa4e17c1c280d0d0ac02d6429ce11bbd25
LimonkaTM <etuzin@mail.ru>
2023-02-23 00:36:33 +0300

    fix lexicon imports

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -2,7 +2,7 @@ from aiogram import Router
 from aiogram.filters import CommandStart
 from aiogram.types import Message
 
-from lexicon.lexicon import LEXICON_RU
+from lexicon.lexicon import LEXICON_BUTTONS, LEXICON_COMANDS, LEXICON_DATA
 
 router: Router = Router()
 
@@ -10,4 +10,4 @@ router: Router = Router()
 # Этот хэндлер будет срабатывать на команду "/start"
 @router.message(CommandStart())
 async def process_start_command(message: Message):
-    await message.answer(LEXICON_RU['buttons'][message.text])
+    await message.answer(LEXICON_COMANDS[message.text])

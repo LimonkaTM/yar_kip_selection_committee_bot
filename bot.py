0bb71dcfa5df07bd1c4f5eaddd67348531952a29
LimonkaTM <etuzin@mail.ru>
2023-02-23 01:12:14 +0300

    rewrote bot endpoint

--- bot.py
+++ bot.py
@@ -4,19 +4,11 @@ import logging
 from aiogram import Bot, Dispatcher
 
 from config_data.config import Config, load_config
-from handlers.other_handlers import register_echo_handler
-from handlers.user_handlers import register_user_handlers
+from handlers import other_handlers, user_handlers
 from keyboards.main_menu import set_main_menu
-
 
 # Инициализируем логгер
 logger = logging.getLogger(__name__)
-
-
-# Фнукция для регистрации всех хэндлеров
-def register_all_handlers(dp: Dispatcher) -> None:
-    register_user_handlers(dp)
-    register_echo_handler(dp)
 
 
 # Функция конфигурирования и запуска бота
@@ -34,20 +26,20 @@ async def main():
     config: Config = load_config()
 
     # Инициализируем бот и диспетчер
-    bot: Bot = Bot(token=config.tg_bot.token, parse_mode='HTML')
-    dp: Dispatcher = Dispatcher(bot)
+    bot: Bot = Bot(token=config.tg_bot.token,
+                   parse_mode='HTML')
+    dp: Dispatcher = Dispatcher()
 
     # Настраиваем главное меню бота
-    await set_main_menu(dp)
+    await set_main_menu(bot)
 
-    # Регистрируем все хэндлеры
-    register_all_handlers(dp)
+    # Регистриуем роутеры в диспетчере
+    dp.include_router(user_handlers.router)
+    dp.include_router(other_handlers.router)
 
-    # Запускаем polling
-    try:
-        await dp.start_polling()
-    finally:
-        await bot.close()
+    # Пропускаем накопившиеся апдейты и запускаем polling
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
 
 
 if __name__ == '__main__':
